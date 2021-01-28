023b606b3527099b8163ba7c3397e8e4f4713160
Satish saini <1995stish@gmail.com>
2021-01-28 13:19:57 +0530

    initial commit

---
+++ src/main/java/com/saini/demo/TestController.java
@@ -0,0 +1,21 @@
+package com.saini.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/")
+	public String about() {
+		return "A spring boot application created by Satish Saini";
+	}
+
+	@GetMapping("/greet/{user}")
+	public String greetUser(@PathVariable("user") String user) {
+		return "Hello " + user;
+
+	}
+	
+}
